a5162d6157ca89e03882a695f4da617186082c8c
Artem <artem15stepanenko@outlook.com>
2024-06-25 18:05:58 +0300

    EndWithForcer

--- Assets/Scripts/Player/PlayerForcer.cs
+++ Assets/Scripts/Player/PlayerForcer.cs
@@ -1,18 +1,57 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerForcer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private bool _isSliding = false;
+
+    [SerializeField] private float _maxForce;
+    [SerializeField] private float _minForce;
+    [SerializeField] private float _sensitivity;
+    [SerializeField] private float _force;
+
+    private Vector2 _startSlidingPoint;
+    private Vector2 _lastMousePosition;
+
+    private Player _player;
+
+    private void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        { 
+            _isSliding = true;
+            _startSlidingPoint = Input.mousePosition;
+        }
+
+        if (_isSliding)
+        {
+            _force += (_lastMousePosition.y - Input.mousePosition.y) * _sensitivity;
+
+            if (_force < _minForce)
+            {
+                _force = _minForce;
+            }
+            else if (_force > _maxForce)
+            {
+                _force = _maxForce;
+            }
+
+            _lastMousePosition = Input.mousePosition;
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            Vector2 direction = _startSlidingPoint - (Vector2)Input.mousePosition;
+
+            _isSliding = false;
+
+            ThrowPlayer(direction.normalized);
+        }
+
+        _lastMousePosition = Input.mousePosition;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void ThrowPlayer(Vector2 direction)
     {
-        
+        _player.Rigidbody.AddForce(direction * _force);
     }
 }
