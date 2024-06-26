4543d339948c0a50195f2d85b123a2ca62d8b07e
Artem <artem15stepanenko@outlook.com>
2024-06-26 22:40:25 +0300

    Added fade and SceneManager

---
+++ Assets/Scripts/Player/PlayerRoamer.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerRoamer : MonoBehaviour
+{
+    [SerializeField] private float _maxForce;
+
+    private Rigidbody2D _rigidbody;
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+        float randomForce = Random.Range(0.0f, _maxForce);
+
+        Vector2 randomDirection = GetRandomDirection();
+
+        _rigidbody.velocity = randomDirection * randomForce;
+    }
+
+    private Vector2 GetRandomDirection() => new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f)).normalized;
+}
